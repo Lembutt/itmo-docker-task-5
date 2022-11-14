0dd4746e718c6f9cbf148ba25a5c512ea237b2f8
Biziarkin Iaroslav S <comradeyar@gmail.com>
2022-11-14 16:10:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 const http = require('http');
-<<<<<<< Updated upstream
-let counter = 0;
-const server = new http.Server();
-server
-    .on('request', (req, res) => {
-        if (req.url == '/') {
-            // (возвращается текущее значение счётчика, инкремента не происходит);
-            res.end(String(counter))
-        } else if (req.url == '/stat') {
-            // (возвращается текущее значение счётчика, и происходит инкремент);
-            res.end(String(counter))
-            counter = counter + 1
-        } else if (req.url == '/about') {
-            // (возвращается html-страничка)
-            res.writeHeader(200, { "Content-Type": "text/html" });
-            html = '<h3> Hello , Biziarkin Iaroslav</h3>'
-            res.write(html);
-            res.end()
-        } else {
-            // все остальное
-            res.end('404')
-        }
-    })
-    .listen(port=8080, hostname='0.0.0.0')
-=======
 const { Client } = require('pg')
 const run = async () => {
     try {
@@ -84,4 +59,3 @@ const run = async () => {
 }
 
 run()
->>>>>>> Stashed changes
